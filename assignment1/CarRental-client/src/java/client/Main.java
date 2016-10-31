c00dc2d8c75165eb9a336703451ca0c6bb2ae358
Elias Iblisdir <elias@macbook-air.home>
2016-10-31 13:12:26 +0100

    implemented and tested Main class closes #7 #8 #9 #10 #11 #12

--- assignment1/CarRental-client/src/java/client/Main.java
+++ assignment1/CarRental-client/src/java/client/Main.java
@@ -1,16 +1,88 @@
 package client;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import rental.CarType;
+import rental.Quote;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import rental.ReservationException;
 import session.CarRentalSessionRemote;
+import session.ManagerSessionRemote;
 
-public class Main {
+
+public class Main extends AbstractTestAgency <CarRentalSessionRemote, ManagerSessionRemote>{
     
     @EJB
     static CarRentalSessionRemote session;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("found rental companies: "+session.getAllRentalCompanies());
+        String file = "simpleTrips";
+        Main client = new Main(file);
+        try {
+            client.run();
+        }catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+       
     }
+
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    @Override @EJB
+    protected CarRentalSessionRemote getNewReservationSession(String name) throws Exception {
+        InitialContext context = new InitialContext();
+        return (CarRentalSessionRemote) context.lookup(CarRentalSessionRemote.class.getName());
+        
+    }
+
+    @Override
+    protected ManagerSessionRemote getNewManagerSession(String name, String carRentalName) throws Exception {
+        InitialContext context = new InitialContext();
+        return (ManagerSessionRemote) context.lookup(ManagerSessionRemote.class.getName());
+    }
+
+    @Override
+    protected void checkForAvailableCarTypes(CarRentalSessionRemote session, Date start, Date end) throws Exception {
+        Set<CarType> cartypes = session.checkForAvailableCarTypes(start,end);
+        for (CarType c: cartypes){
+            System.out.println(c.toString());
+        }
+    }
+
+    @Override
+    protected void addQuoteToSession(CarRentalSessionRemote session, String name, Date start, Date end, String carType, String region) throws Exception {
+        ReservationConstraints constraints = new ReservationConstraints(start, end, carType, region);
+        session.createQuote(constraints, name);
+    }
+
+    @Override
+    protected List<Reservation> confirmQuotes(CarRentalSessionRemote session, String name) throws Exception {
+        return session.confirmQuotes(name);
+        
+    }
+
+    @Override
+    protected int getNumberOfReservationsForCarType(ManagerSessionRemote ms, String carRentalName, String carType) throws Exception {
+        return ms.getNumberOfReservationsForCarType(carRentalName,carType);
+    }
+    
+    @Override
+    protected int getNumberOfReservationsBy(ManagerSessionRemote ms, String renter){
+        return ms.getNumberOfReservationsBy(renter);
+    }
+    
 }
